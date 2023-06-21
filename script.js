acb326465abe9a970d3267bc5f1545928e1a2875
Michael Aurilia <michaelaurilia919@gmail.com>
2023-06-21 15:22:16 -0400

    Finished Code

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
 // Retrieve DOM elements
-// TODO: Get references to the required DOM elements using document.getElementById()
+const posterImage = document.getElementById('poster-image');
+const posterTitle = document.getElementById('poster-title');
+const posterQuote = document.getElementById('poster-quote');
+const randomizeButton = document.getElementById('randomize');
+const submitButton = document.getElementById('submit');
+const quoteInput = document.getElementById('poster-form-quote');
 
 // Array of predefined poster objects
 const posters = [
   {
-    image: 'image-url-1.jpg',
-    title: 'Poster 1',
-    quote: 'Quote 1',
+    imageSrc: 'https://img.fruugo.com/product/6/75/21266756_max.jpg',
+    title: 'Achievement',
+    quote: 'This is one small step for man, One giant leap for mankind.',
   },
   {
-    image: 'image-url-2.jpg',
-    title: 'Poster 2',
-    quote: 'Quote 2',
+    imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4Ux6A3Ajks4n_dR6iT-tORkBd3CIsPD7uLA&usqp=CAU',
+    title: 'Determination',
+    quote: 'What lies behind us and what lies ahead of us are tiny matters compared to what lives within us.',
   },
   {
-    image: 'image-url-3.jpg',
-    title: 'Poster 3',
-    quote: 'Quote 3',
+    imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReeh-CsyzzNG4NuIpAporr-V891fTAadBLLg&usqp=CAU',
+    title: 'Leadership',
+    quote: 'The Supreme Quality For Leadership Is Unquestionable Integrity. Without It, No Real Success Is Possible.',
   },
   // Add more poster objects as needed
 ];
 
 // Event listener for randomize button
-// TODO: Add an event listener to the randomizeButton that calls a function when clicked
+randomizeButton.addEventListener('click', generateRandomPoster);
 
 // Event listener for submit button
-// TODO: Add an event listener to the submitButton that calls a function when clicked
+submitButton.addEventListener('click', generateCustomPoster);
 
 // Function to generate a random poster
 function generateRandomPoster() {
-  // TODO: Generate a random index within the range of the posters array length
-
-  // TODO: Retrieve the random poster object from the posters array
-
-  // TODO: Call the function to update the DOM with the values from the random poster object
+  const randomIndex = Math.floor(Math.random() * posters.length);
+  const randomPoster = posters[randomIndex];
+  updatePoster(randomPoster);
 }
 
 // Function to generate a custom poster
 function generateCustomPoster(event) {
-  event.preventDefault();
-
-  // TODO: Retrieve the entered quote and author from the input fields
-
-  // TODO: Create a custom poster object with the entered values
-
-  // TODO: Call the function to update the DOM with the values from the custom poster object
-}
+    event.preventDefault();
+    const customQuote = quoteInput.value || 'No quote provided';
+    const customTitle = titleInput.value || 'No title provided';
+    const customPoster = {
+      imageSrc: 'https://cdn3.successories.com/products/6611969dz-751680-motivational-posters-integrity-island-motivational-poster.jpg',
+      title: customTitle,
+      quote: customQuote,
+    };
+    updatePoster(customPoster);
+  }
 
 // Function to update the poster content in the DOM
-function updatePoster(imageUrl, title, quote) {
-  // TODO: Update the DOM with the values provided for the poster image, title, and quote
+function updatePoster(poster) {
+  posterImage.src = poster.imageSrc;
+  posterTitle.textContent = poster.title;
+  posterQuote.textContent = poster.quote;
 }
